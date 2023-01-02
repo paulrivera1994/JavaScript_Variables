64fc0f1944d374b3c69c38585a6c773780fd221f
paulrivera1994 <100796671+paulrivera1994@users.noreply.github.com>
2023-01-02 11:00:18 -0700

    Finished Assignment

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
-
+let firstName;
+let lastName = "Rivera";
+var age = 28;
 // Exercise 2
-
+firstName = "Paul";
+lastName = "Rivera";
+age = 28;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+// Exercise 5
+let title = "Name of the Wing";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
